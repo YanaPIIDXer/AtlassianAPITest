113010608df9859360226e80f85467f1c161f83b
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2022-04-02 23:09:16 +0900

    Vue-Routerの設定

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import dotenv from 'dotenv'
 import store from './store'
+import router from './router'
 
 dotenv.config()
 
@@ -9,5 +10,6 @@ Vue.config.productionTip = false
 
 new Vue({
   store,
+  router,
   render: h => h(App),
 }).$mount('#app')
